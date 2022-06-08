06501cf25d9d7ffe7b98ff05c960980564f7965b
wengqi <wengqi925@163.com>
2022-06-08 11:39:44 +0800

    调整文档这 改写 Cesium.when

--- src/page/stallite.jsx
+++ src/page/stallite.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
 import { createRoot } from 'react-dom/client';
+import * as turf from '@turf/turf';
 import GisMap from '../code/gisMap';
 import './index.less';
 // window['CESIUM_BASE_URL'] = '/static/Cesium'
 const gisMap = new GisMap('cesium');
 
+// const { Point } = turf;
+// console.log(turf, turf.point([120, 30]));
 gisMap.setView({
   longitude: 106.038795,
   latitude: 31.042339,
@@ -28,7 +31,7 @@ function Content() {
     console.log(result, 33);
   };
   const getSize = () => {
-    const _measure = gisMap.measurePolygn(gisMap.viewer);
+    const _measure = gisMap.measurePolygn();
     setMeasure(_measure);
   };
   const removeSize = () => {
@@ -36,6 +39,18 @@ function Content() {
     console.log(result, 22223);
   };
 
+  const selectRectCallBack = (data) => {
+    console.log(data);
+  };
+
+  const selectRect = () => {
+    const _measure = gisMap.selectRect({ onFinsh: selectRectCallBack });
+    setMeasure(_measure);
+  };
+
+  const removeAll = () => {
+    gisMap.removeAll();
+  };
   return (
     <div className="box">
       <div className="btn" role="none" onClick={() => getImage()}>截图</div>
@@ -43,6 +58,8 @@ function Content() {
       <div className="btn" role="none" onClick={() => removeDistance()}>完成测量距离</div>
       <div className="btn" role="none" onClick={() => getSize()}>测量面积</div>
       <div className="btn" role="none" onClick={() => removeSize()}>完成测量面积</div>
+      <div className="btn" role="none" onClick={() => selectRect()}>框选</div>
+      <div className="btn" role="none" onClick={() => removeAll()}>清空所有</div>
     </div>
   );
 }
